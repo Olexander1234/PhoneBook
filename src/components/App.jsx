4c870ccf839ea1f4f2d9d24fb2265329138daea9
Olexander <cmurtimur@gmail.com>
2023-09-15 18:26:14 +0300

    яви

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,104 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from "react";
+import { nanoid } from "nanoid";
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ],
+    name: "",
+    number: "",
+    filter: "",
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.currentTarget.name]: e.currentTarget.value,
+    });
+  };
+
+  isNameAlreadyExists = (name) => {
+    return this.state.contacts.some((contact) => contact.name === name);
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { name, number } = this.state;
+
+    // Перевірка, чи ім'я вже існує
+    if (this.isNameAlreadyExists(name)) {
+      alert(`${name} is already in contacts.`);
+      return;
+    }
+
+    const newContact = { id: nanoid(), name, number };
+    this.setState(({ contacts }) => {
+      return { contacts: [...contacts, newContact] };
+    });
+  };
+
+  handleFilter = (e) => {
+    this.setState({
+      filter: e.currentTarget.value,
+    });
+  };
+
+  getVisibleContacts = () => {
+    return this.state.contacts.filter((contact) => {
+      return contact.name.includes(this.state.filter);
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Phone Book</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Number
+            <input
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+              onChange={this.handleChange}
+            />
+          </label>
+          <button type="submit">Add contact</button>
+        </form>
+        <label>
+          Find contacts by name
+          <input onChange={this.handleFilter} type="text" />
+        </label>
+
+        <h2>Contacts</h2>
+        <ul>
+          {this.getVisibleContacts().map(({ name, number }) => {
+            return (
+              <li key={nanoid()}>
+                <p>
+                  {name}: {number}
+                </p>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
